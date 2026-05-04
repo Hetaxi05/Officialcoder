dd1f4623948c9ff8c4911b435456f625a72096d6
Hetaxi05 <hetaxinavadiya14@gmail.com>
2026-05-04 10:09:57 +0100

    Update activities.js

--- api/router/activities.js
+++ api/router/activities.js
@@ -3,24 +3,6 @@ const RouterActivity = express.Router();
 const Activity = require("../model/activity");
 const { body, validationResult } = require("express-validator");
 
-// ✅ 1. Log activity (insert userId and timestamp)
-// RouterActivity.post("/log", [
-//     body("userId").notEmpty().withMessage("User ID is required"),
-// ], async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//     }
-
-//     try {
-//         const { userId } = req.body;
-//         const activity = await Activity.create({ userId });
-//         res.status(201).json({ message: "Activity logged successfully", activity });
-//     } catch (error) {
-//         res.status(500).json({ message: "Failed to log activity", error });
-//     }
-// });
-
 RouterActivity.get("/all", async (req, res) => {
     try {
       const activityLogs = await Activity.find().populate("userId", "email"); // populate email from User
@@ -30,7 +12,7 @@ RouterActivity.get("/all", async (req, res) => {
     }
   });
 
-// 📊 2. Get activity count grouped by date
+//Get activity count grouped by date
 RouterActivity.get("/status", async (req, res) => {
     try {
         const data = await Activity.aggregate([
